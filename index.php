41c02415fede4988bb75f3f83a263c88a492f5f2
AmerA10 <ajami.amer1@gmail.com>
2021-03-29 14:00:02 -0600

    commit to heroku

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
         <header class="navbar">
             <div class="currentPage">
-                <a href="index.php">Home</a>
+                <a href="index.php">adasdasdasdasd</a>
                 <i class="fa fa-bars menuIcon"></i>
             </div>
             
