03c857d19a6141aca57c2e87da2f532c04bd2967
AmerA10 <ajami.amer1@gmail.com>
2021-03-29 23:01:03 -0600

    almsot done with click stuff

--- js/singleCompany.js
+++ js/singleCompany.js
@@ -11,38 +11,12 @@ fetch(companyAPI)
         })
     .then( data => {
         companies.push(...data);
-        populateCompanyList(companies);
-        MakeClickable();
+        console.log(companies);
+       
     })
     .catch( error => console.log(`found a ${error}`) );
 
 /* Adds each company either from local storage or API to a list, also adding
 event delegation to the list of company (refer to displayInformation) */
-function populateCompanyList(companies) {
-    companies.forEach( company => {
-        let li = document.createElement('li');
-        li.innerHTML = `<div class='list-item-section1'><img src="logos/${company.symbol}.svg" style="width:60px;height:60px"></div>` 
-        + `<div class='list-item-section2'><a href = single-company.php?symbol=${company.symbol}>${company.symbol}</div>`
-        + `<div class='list-item-section3'><a href = single-company.php?symbol=${company.symbol}>${company.name}</div>`;
-        companyList.appendChild(li);
-    });
-    setTimeout( () => {
-        document.querySelector('.sk-circle').style.display = "none";
-        companyList.style.display = "inline-block";
-    }, 1000);
-}
 
-function MakeClickable() {
-  let list = companyList.querySelectorAll('li');
-    for(company of list){
-        let symbol = company.querySelector('.list-item-section3');
-        let name = company.querySelector('.list-item-section2');
-        symbol.addEventListener('click', (e)=> {
-            console.log("you clicked text" + e.target.innerHTML);           
-        });
-        name.addEventListener('click', (e)=> {
-            //todo get symbol from the name           
-        });
-        
-    };
-}
+
