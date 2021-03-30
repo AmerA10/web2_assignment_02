d1a35f7e02be3dd40a68478ef58e1f8f0496a042
mzein694 <mzein694@mtroyal.ca>
2021-03-30 00:20:45 -0600

    reverting it back

--- about.php
+++ about.php
@@ -25,7 +25,6 @@
                 <a href="profile.php">Profile</a>
                 <a href="favorites.php">Favorites</a>
                 <a href="logout.php">Logout</a>
-                <a href="logout.php">test</a>
             </div>
         </header>
     </body>
