0b2f99ada3b169ee4c6e5c5abcf84d0be8ff2820
AmerA10 <ajami.amer1@gmail.com>
2021-03-29 23:13:47 -0600

    almsot done with click stuff

--- single-company.php
+++ single-company.php
@@ -1,5 +1,7 @@
 <?php
-
+if(isset([$_GET['symbol']])) {
+    echo "symbol is set" . $_GET['symbol'];
+}
 
 ?>
 <!DOCTYPE html>
@@ -7,6 +9,7 @@
 <head>
     <meta charset="utf-8"/>  
     <title>Assignment #2</title>   
+    <script src ="js/singleCompany.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
     <link rel="stylesheet" href="css/main.css">
@@ -35,5 +38,5 @@
 
     </body>
     <script src="js/main.js"></script>
-    <script src ="js/singleCompany.js"></script>
+    
 </html>
