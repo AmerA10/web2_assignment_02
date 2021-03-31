a371502db1c97ce89a6beb4ce1d20c6492cb2515
Mohamed Zeineldin <59937388+mzein694@users.noreply.github.com>
2021-03-31 02:04:26 -0600

    Add files via upload
    content added in login page

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <title>Assignment #2</title>   
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
@@ -27,6 +28,29 @@
                 <a href="logout.php">Logout</a>
             </div>
         </header>
+
+        <div class="container">
+    
+    <p>Please fill in your account info</p>
+    
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw"  min="7" max="15" required>
+
+    <a href="login.php"><button type="button" class="cancelbtn">login</button></a>
+    <h3 id= "noacc">No Account?</h3>
+    <a href="signup.php"><button type="button" class="signup">Sign Up</button></a>
+    
+      
+    </div>
+  </div>
+</form>
+
+
     </body>
     <script src="js/main.js"></script>
 </html>
