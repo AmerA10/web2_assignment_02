c7cfbaad170afa249ff68b1c0dec3e95249eb53c
AmerA10 <ajami.amer1@gmail.com>
2021-04-04 16:06:33 -0600

    fixing single-company

--- single-company.php
+++ single-company.php
@@ -15,7 +15,7 @@ else  {
 <head>
     <meta charset="utf-8"/>  
     <title>Assignment #2</title>   
-    <script src="js/singleCompany.js"></script>
+    
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
     <link rel="stylesheet" href="css/main.css">
@@ -49,5 +49,6 @@ else  {
 
     </body>
     <script src="js/main.js"></script>
+    <script src="js/singleCompany.js"></script>
     
 </html>
