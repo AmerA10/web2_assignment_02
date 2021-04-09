bf7e2ede9b730a5dd9ef9da79175fe615cdfe907
Matthew Fudge <mfudg395@mtroyal.ca>
2021-04-09 13:33:16 -0600

    update markup & styling for favorites & history buttons

--- single-company.php
+++ single-company.php
@@ -61,8 +61,8 @@
             </span>
             <br/>
             <div class="buttonContainer">
-                <button class="favoritesButton">Add to Favorites</button>
-                <button class="historyButton">History</button>
+                <a href='addtofav.php'>Add to Favorites</a>
+                <a href='history.php?symbol=<?=$company['symbol']?>'>History</a>
             </div>
         </div>
     </body>
