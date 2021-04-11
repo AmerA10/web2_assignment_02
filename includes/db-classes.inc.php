6bf9ca14bb6166806b7e76aac91aa23f78bf5b3e
AmerA10 <ajami.amer1@gmail.com>
2021-04-10 23:28:49 -0600

    testing portfolio

--- includes/db-classes.inc.php
+++ includes/db-classes.inc.php
@@ -57,7 +57,7 @@ class PortfolioDB {
         
       // $sql = "SELECT portfolio.userId, portfolio.amount, portfolio.symbol 
       // FROM portfolio LEFT JOIN users On portfolio.userId = users.id WHERE portfolio.userId=?";
-      $sql = "SELECT * from porfolio";
+       $sql = "SELECT * from portfolio";
        $statement = DatabaseHelper::runQuery($this->pdo, $sql, null);
        return $statement->fetchAll();
     }
