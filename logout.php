93aae15fd7a8c6f5e3cdc1a7c46a4a1f646711a9
AmerA10 <ajami.amer1@gmail.com>
2021-04-12 16:39:45 -0600

    porftolio basic

--- logout.php
+++ logout.php
@@ -3,22 +3,21 @@ session_start();
 
 
 
-if(isset($_SESSION["loggedin"]) && $_SESSION['loggedin']) {
-       
-        if(isset($_SESSION['userId'])) {
-        // session_unset($_SESSION['userId']);
-         echo "this page works..";
-        }
-//     if(isset($_SESSION['userEmail'])) {
-//         session_unset($_SESSION['userEmail']);
-//     }
+if (isset($_SESSION["loggedin"]) && $_SESSION['loggedin']) {
 
-//     header("location: index.php");
-$_SESSION["loggedin"] = false;
-echo "this page works..";
+    if (isset($_SESSION['userId'])) {
+        session_unset($_SESSION['userId']);
+        echo "this page works..";
+    }
+    if (isset($_SESSION['userEmail'])) {
+        session_unset($_SESSION['userEmail']);
+        echo "this page works..";
+    }
+
+    //     header("location: index.php");
+    $_SESSION["loggedin"] = false;
+    echo "this page works..";
 }
 // else {
 //     header("location: index.php");
 // }
-
-?>
