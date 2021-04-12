7021d15717b5cae168316e752f3918a2ad00f869
AmerA10 <ajami.amer1@gmail.com>
2021-04-12 16:41:05 -0600

    porftolio basic

--- logout.php
+++ logout.php
@@ -9,10 +9,10 @@ if (isset($_SESSION["loggedin"]) && $_SESSION['loggedin']) {
         session_unset($_SESSION['userId']);
         echo "this page works..";
     }
-    if (isset($_SESSION['userEmail'])) {
-        session_unset($_SESSION['userEmail']);
-        echo "this page works..";
-    }
+   // if (isset($_SESSION['userEmail'])) {
+   //     session_unset($_SESSION['userEmail']);
+    //   echo "this page works..";
+//}
 
     //     header("location: index.php");
     $_SESSION["loggedin"] = false;
