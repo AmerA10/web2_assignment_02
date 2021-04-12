281ba278bab8fb0789c94cfd39432d1004a8ee3f
AmerA10 <ajami.amer1@gmail.com>
2021-04-12 14:38:59 -0600

    more updates

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
-//just have to switch '!'
 session_start();
+//just have to switch '!'
 if(isset($_SESSION["loggedin"])) {
     $_SESSION["loggedin"] = false;
     header("location: index.php");
+    session_destroy();
 }
-?>
