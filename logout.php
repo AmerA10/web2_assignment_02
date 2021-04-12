914d2b11bad108c510c21c4e08907ba53e612a59
AmerA10 <ajami.amer1@gmail.com>
2021-04-12 16:34:55 -0600

    porftolio basic

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ session_start();
 
 
 if(isset($_SESSION["loggedin"]) && $_SESSION['loggedin']) {
-//     $_SESSION["loggedin"] = false;
+        $_SESSION["loggedin"] = false;
 //     if(isset($_SESSION['userId'])) {
 //         session_unset($_SESSION['userId']);
 //     }
