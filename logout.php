b13ea12b9b0469faee46aec170673473dbce2d6e
AmerA10 <ajami.amer1@gmail.com>
2021-04-13 15:54:33 -0600

    fixing table

--- logout.php
+++ logout.php
@@ -1,19 +1,9 @@
 <?php
 session_start();
-
-
-
 if (isset($_SESSION["loggedin"]) && $_SESSION['loggedin']) {
 
-    if (isset($_SESSION['userId'])) {
-        unset($_SESSION['userId']);
-    
-    }
-    if (isset($_SESSION['userEmail'])) {
-        unset($_SESSION['userEmail']);
-        
-    }
-    $_SESSION["loggedin"] = false;
+    session_destroy();
+
     header("location: index.php");
 } else {
     header("location: index.php");
