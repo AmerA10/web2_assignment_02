e00e536ddd385b251cb4a2904e11054a21e740e6
AmerA10 <ajami.amer1@gmail.com>
2021-04-12 23:17:56 -0600

    company sorting

--- portfolio.php
+++ portfolio.php
@@ -52,6 +52,7 @@ try {
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel= "stylesheet" href="css/portfolio.css">
 </head>
 
 <body>
