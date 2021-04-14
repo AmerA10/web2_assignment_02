3f9c2b72bf98305bf07aa92a28f433249250af2b
AmerA10 <ajami.amer1@gmail.com>
2021-04-14 00:27:18 -0600

    finishing touches

--- favorites.php
+++ favorites.php
@@ -47,7 +47,7 @@ function createFavList() {
                     <li><a href="about.php">About</a></li>
                     <li><a href="list.php">Companies</a></li>
                     <?php
-                    if (isset($_SESSION[("loggedin")]) && $_SESSION['loggedin']) {
+                    if (isset($_SESSION["loggedin"]) && $_SESSION['loggedin']) {
                     
                             echo "<li><a href='portfolio.php'>Portfolio</a></li>";
                             echo "<li><a href='profile.php'>Profile</a></li>";
