882b7c2461a25b08f9466ae453306c8814c03358
AmerA10 <ajami.amer1@gmail.com>
2021-04-13 23:34:44 -0600

    doing portfolio stuff

--- about.php
+++ about.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet", href="css/about.css">
+    <link rel="stylesheet" href="css/about.css">
 </head>
 <body>
         <header class="navbar">
