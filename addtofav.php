0522a0bf5b66e900aa0643e1f3aa56892a9146e5
AmerA10 <ajami.amer1@gmail.com>
2021-04-13 23:21:40 -0600

    doing portfolio stuff

--- addtofav.php
+++ addtofav.php
@@ -1,32 +1,35 @@
 <?php
-include 'includes/helpers.inc.php';
-include 'includes/db-classes.inc.php';
-include 'includes/stock-config.inc.php';
+    include 'includes/helpers.inc.php';
+    include 'includes/db-classes.inc.php';
+    include 'includes/stock-config.inc.php';
 
-session_start();
+    session_start();
 
-$companiesGateway = new CompanyDB($connection);
+    $companiesGateway = new CompanyDB($connection);
 
-if (!isset($_SESSION["fav"])) {
-    $_SESSION["fav"] = [];
-}
-$fav = $_SESSION["fav"];
+    if(!isset($_SESSION["fav"])) {
+        $_SESSION["fav"] = [];
+    }
+    $fav = $_SESSION["fav"];
 
 
-for ($i = 0; $i < count($fav); $i++) {
-    foreach ($fav[$keys[$i]] as $array) {
-        if ($_GET['symbol '] == $array['symbol']) {
+    for($i = 0; $i < count($fav); $i++) {
+        foreach ($fav[$keys[$i]] as $array) {
+           if($_GET['symbol '] == $array['symbol']){
             header("location: " . $_SERVER["HTTP_REFERER"]);
             exit();
-        } else {
-            $fav[] = $companiesGateway->getAllForCompany($_GET["symbol"]);
-
-            $_SESSION["fav"] = $fav;
-
-
-
-            $conn = null;
-            header("location: " . $_SERVER["HTTP_REFERER"]);
+           }  
         }
     }
-}
+    
+
+    $fav[] = $companiesGateway->getAllForCompany($_GET["symbol"]);
+    
+    $_SESSION["fav"] = $fav;
+    print_r($fav);
+
+    //session_destroy();
+
+    $conn = null;
+    header("location: " . $_SERVER["HTTP_REFERER"]);
+?>
